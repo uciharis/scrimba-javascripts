5d786c740eb681447450e7b4ff222fdb21190616
bebek <harisnop@gmail.com>
2024-04-23 21:42:28 +0700

    delete fucking useless folder

--- passenger-counter/welcome-message/app.js
+++
@@ -1,8 +0,0 @@
-let welcom = document.getElementById('welcome-el')
-console.log(welcom)
-let nama = "Soeharto"
-let greeting = "welcome beks "
-
-
-welcom.innerText = greeting + nama ;
-welcom.innerText += "💰"
