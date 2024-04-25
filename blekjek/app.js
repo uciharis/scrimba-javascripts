98ece5df14bd11c02ed0db43953e216b6c33f78c
harisnop.koding <harisnop.koding@gmail.com>
2024-04-25 23:16:05 +0700

    refactor console.log into variabel message. also put style css

--- blekjek/app.js
+++ blekjek/app.js
@@ -1,17 +1,20 @@
 let firstCard = 10;
-let secondCard = 19;
+let secondCard = 11;
 let sum = firstCard + secondCard;
 let hasBlekjek = false;
 let isAlive = true;
 
+let message = '';
+
 if (sum <=20){
-    console.log('tarik kartu lagi bos ?')
-} else if (sum == 21){
-    console.log('lu orang MENANG !! 🎉🎉')
+    message = 'tarik kartu lagi bos ?'
+} else if (sum === 21){
+    message = 'lu orang MENANG !! 🎉🎉'
     hasBlekjek = true; // ini state
 } else {
-    console.log('tolol kalah lu tol ..')
+    message = 'tolol kalah lu tol ..'
     isAlive = false; // ini state
 }
 
 console.log(isAlive);
+console.log(message);
