1477871ce7b08e8b09fed396486d07ceba446d97
harisnop.koding <harisnop.koding@gmail.com>
2024-04-25 21:13:48 +0700

    bikin aplikasi blekjek terminal

---
+++ blekjek/app.js
@@ -0,0 +1,11 @@
+let firstCard = 10;
+let secondCard = 7;
+let sum = firstCard + secondCard;
+
+if (sum <=20){
+    console.log('tarik kartu lagi bos ?')
+} else if (sum == 21){
+    console.log('lu orang MENANG !! 🎉🎉')
+} else {
+    console.log('tolol kalah lu tol ..')
+}
