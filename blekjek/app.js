debabab71f39d5a6a8a66fc2c47df8d1d1840cd5
uciharis <harisnop@gmail.com>
2024-04-30 21:40:30 +0700

    tambah dom ke cardBoard

--- blekjek/app.js
+++ blekjek/app.js
@@ -5,17 +5,21 @@ let hasBlekjek = false;
 let isAlive = true;
 
 let message = '';
-let messageBoard = document.getElementById('message')
-let cardBoard = document.getElementById('carBoard')
-let sumBoard = document.getElementById('sumBoard')
-if (sum <=20){
-    message = 'tarik kartu lagi bos ?'
-} else if (sum === 21){
-    message = 'lu orang MENANG !! 🎉🎉'
-    hasBlekjek = true; // ini state
-} else {
-    message = 'tolol kalah lu tol ..'
-    isAlive = false; // ini state
+let messageBoard = document.getElementById('message');
+let cardBoard = document.getElementById('carBoard');
+let sumBoard = document.getElementById('sumBoard');
+
+function startGame(){
+    cardBoard.textContent = 'Kartoe : '
+    if (sum <=20){
+        message = 'tarik kartu lagi bos ?'
+    } else if (sum === 21){
+        message = 'lu orang MENANG !! 🎉🎉'
+        hasBlekjek = true; // ini state
+    } else {
+        message = 'tolol kalah lu tol ..'
+        isAlive = false; // ini state
+    }
 }
 
 console.log(isAlive);
