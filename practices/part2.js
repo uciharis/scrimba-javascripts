db1a3ae51563e758cd3e2ba7c364139c05f11032
uciharis <harisnop@gmail.com>
2024-05-04 23:11:02 +0700

    reformatting console.log into the fit space for text content

---
+++ practices/part2.js
@@ -0,0 +1,11 @@
+let person = {
+    name : 'jowo ki',
+    age : 69,
+    country : 'solo sin city'
+};
+
+function logData(){
+    console.log(person.name+' is '+person.age+' years old and lives in '+person.country)
+}
+
+logData();
