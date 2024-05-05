51221efada595933b42499245da7ddc77c1def1c
uciharis <harisnop@gmail.com>
2024-05-05 11:06:31 +0700

    if else with 'and' & 'no-and'

--- practices/part2.js
+++ practices/part2.js
@@ -1,13 +1,52 @@
-// let person = {
-//     name : 'jowo ki',
-//     age : 69,
-//     country : 'solo sin city'
-// };
+/* ----- object -----------
 
-// function logData(){
-//     console.log(person.name+' is '+person.age+' years old and lives in '+person.country)
-// }
+let person = {
+    name : 'jowo ki',
+    age : 69,
+    country : 'solo sin city'
+};
 
-// logData();
+function logData(){
+    console.log(person.name+' is '+person.age+' years old and lives in '+person.country)
+}
 
-let age = 15;
+logData();
+
+*/
+
+// 
+/*
+----------- if else cara #1------------------
+
+let age = 75;
+if (age < 6){
+    console.log('free')
+} else if(age <17 && age >6){
+    console.log('child disc')
+} else if(age <26 && age > 18){
+    console.log('student disc')
+} else if(age <66 && age >27){
+    console.log('full price')
+} else {
+    console.log('senior citizen disc')
+}
+
+*/
+
+/*
+----------- if else cara #2------------------
+
+let age = 65;
+if (age <6){
+    console.log('free')
+} else if (age<18){
+    console.log('child disc')
+} else if(age<27){
+    console.log('student disc')
+} else if(age<67){
+    console.log('full price')
+} else {
+    console.log('senior citizen disc')
+}
+
+*/
