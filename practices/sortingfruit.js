f6e8dfb26647936170e3cb8b8c1cb225dfacfa7d
uciharis <harisnop@gmail.com>
2024-05-10 22:41:47 +0700

    ganti textContent ke innerText

--- practices/sortingfruit.js
+++ practices/sortingfruit.js
@@ -1,1 +1,15 @@
 let fruit = ["apel", "jeruk", "apel", "apel", "jeruk"];
+let apel = document.getElementById('kotakapel');
+let jeruk = document.getElementById('kotakjeruk');
+
+function putKotak(){
+    for (let i=0;i<fruit.length;i++){
+        if (fruit[i] === 'apel'){
+            apel.innerText += ' '+'apel'
+        } else {
+            jeruk.innerText += ' '+'jeruk'
+        }
+    }
+}
+
+putKotak();
