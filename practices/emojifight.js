8e9ce80ebdffbadf7d3d5f9ee6b5965094c98682
harisnop.koding <harisnop.koding@gmail.com>
2024-05-10 09:10:27 +0700

    nambah objek stage dan fightButton, nambah trigger click utk menjalankan fungsi random

--- practices/emojifight.js
+++ practices/emojifight.js
@@ -1,1 +1,9 @@
 let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+let stage = document.getElementById('stage')
+let fightButton = document.getElementById('fight')
+
+fightButton.addEventListener('click',function(){
+    let randomIndex = Math.floor(Math.random() * fighters.length)
+    console.log(randomIndex)
+})
