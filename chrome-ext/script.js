f0f471a69e48098f082e05a75179eee6b76a3e5c
uciharis <harisnop@gmail.com>
2024-06-18 19:55:33 +0700

    perbaikan + child li

--- chrome-ext/script.js
+++ chrome-ext/script.js
@@ -1,8 +1,18 @@
-let myLeads = [];
+let myLeads = ['a.com','b.com','c.com']
 
-const inputElement = document.getElementById('input-element');
+const inputElement = document.getElementById('input-element').value;
 const simpanBtn = document.getElementById('simpan-btn');
+const ulElement = document.getElementById('ul-element');
 
 simpanBtn.addEventListener('click', function(){
-console.log('klak klik')
+myLeads.push(inputElement);
+console.log(inputElement);
 })
+
+
+for (let data of myLeads){
+    console.log(data);
+    let liElement = document.createElement('li');
+    liElement.textContent +=data;
+    ulElement.appendChild(liElement);
+}
