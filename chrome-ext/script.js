6f5478d887a60e2ccdf08b7d0f9ebc810dc1f34e
uciharis <harisnop@gmail.com>
2024-06-20 00:35:24 +0700

    some ways from teacher scrimba

--- chrome-ext/script.js
+++ chrome-ext/script.js
@@ -9,15 +9,26 @@ myLeads.push(inputElement);
 console.log(inputElement);
 })
 
-// cara sendiri
-// for (let data of myLeads){
-//     console.log(data);
-//     let liElement = document.createElement('li');
-//     liElement.textContent +=data;
-//     ulElement.appendChild(liElement);
-// }
+ //cara sendiri
+ for (let data of myLeads){
+     console.log(data);
+     let liElement = document.createElement('li');
+     liElement.textContent +=data;
+     ulElement.appendChild(liElement);
+ }
 
-//cara master scrimba
+ /*
+// cara master scrimba- old ways
 for (let i=0 ;i<myLeads.length; i++){
     ulElement.innerHTML += '<li>' + myLeads[i] + '</li>';
 }
+*/
+
+/*
+// cara master scrimba- old ways (improved)
+let listItem = '';
+for (let i=0 ;i<myLeads.length; i++){
+    listItems += '<li>' + myLeads[i] + '</li>';
+}
+ulElemet.innerHTML = listItems; // faster bcoz outside the loop
+*/
