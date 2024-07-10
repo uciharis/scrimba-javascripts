25c7757c7d0a15356d0c2c8e5a0a2ceae06e3ff1
uciharis <harisnop@gmail.com>
2024-07-10 10:06:07 +0700

    refactor inputField value ke inputFieldValue

--- mobileApp/app.js
+++ mobileApp/app.js
@@ -1,5 +1,6 @@
 const addBtn = document.getElementById('add-btn');
 const inputField = document.getElementById('input-field');
 addBtn.addEventListener('click', function(){
-    console.log(inputField.value);
+    let inputFieldValue = inputField.value;
+    console.log(inputFieldValue);
 })
