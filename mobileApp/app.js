6bc40c9810b11e249179a072a1ed3dae8ced0b05
uciharis <harisnop@gmail.com>
2024-07-10 11:22:06 +0700

    fixing initialization

--- mobileApp/app.js
+++ mobileApp/app.js
@@ -1,13 +1,14 @@
 import { add } from "./function.js";
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-
 const appSettings = {
     databaseURL: 'https://kucingchartshop-default-rtdb.asia-southeast1.firebasedatabase.app/'
 }
+const app = initializeApp(appSettings);
 
 // ----------------------- //
 let jumlah = add(1,3)
 console.log(jumlah)
+console.log(app)
 
 
 const addBtn = document.getElementById('add-btn');
