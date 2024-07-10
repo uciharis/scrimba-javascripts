451939f8559056f98c8173b29d8cf89e19df7815
uciharis <harisnop@gmail.com>
2024-07-10 10:03:28 +0700

    eventlistener ke inputField

---
+++ mobileApp/app.js
@@ -0,0 +1,5 @@
+const addBtn = document.getElementById('add-btn');
+const inputField = document.getElementById('input-field');
+addBtn.addEventListener('click', function(){
+    console.log(inputField.value);
+})
