db21a5d714f67641499749d33bb2a00586884051
uciharis <harisnop@gmail.com>
2024-07-10 10:57:51 +0700

    adding firebase URL reference

--- mobileApp/app.js
+++ mobileApp/app.js
@@ -1,3 +1,7 @@
+const appSettings = {
+    databaseURL: 'https://kucingchartshop-default-rtdb.asia-southeast1.firebasedatabase.app/'
+}
+
 const addBtn = document.getElementById('add-btn');
 const inputField = document.getElementById('input-field');
 addBtn.addEventListener('click', function(){
